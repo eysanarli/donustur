bd962733d584d6289b2eafa806291742b0bccf74
Eyşan Arlı <NsA.94@hotmail.com>
2021-01-20 23:32:08 +0300

    Proje dosyası ekle.

---
+++ donustur/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace donustur
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lütfen ekrana bir yazı giriniz");
+            string lettering = Console.ReadLine();
+            Console.WriteLine("Yazdığınız yazı " + lettering);
+            int number = lettering.Length;
+            Console.WriteLine("Yazdığınız yazının harf sayısı " + number);
+            string uppercase = lettering.ToUpper();
+            Console.WriteLine("Yazdığınız yazının büyük harfli yazımı " + uppercase);
+
+            string intentiation = "";
+            for (int i = 0; i < lettering.Length; i++)
+            {
+               intentiation+= (lettering[i]).ToString() .ToUpper() + ".";
+
+                
+              
+            }
+
+            Console.WriteLine("Yazdığınız yazının boşluklu yazımı " + intentiation);
+            
+            
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
